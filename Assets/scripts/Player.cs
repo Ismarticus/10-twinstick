4123deedcc68ae6674e3099ba48ecd9f560e60b5
TJ Rowe <planktime@gmail.com>
2017-06-30 17:08:41 -0600

    Wow that is a big commit
    I have a class script in here as well as work from the follow lessons
    
    Lesson 268 271 271

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,15 +5,30 @@ using UnityStandardAssets.CrossPlatformInput;
 
 
 public class Player : MonoBehaviour {
+    //setting a float so I can control the speed of the movement from the unity gui.
     public float MovZ;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // This also works.  This time however there is a small delay for the "gravity" settings.  This has a neat effect like a thruster.
+    void Update()
+    {
+        //Working on building a move horizontal method. 
+        Debug.Log(Input.GetAxis("Horizontal"));
+        if (CrossPlatformInputManager.GetAxis("Horizontal") == 1f)
+        {
+            this.transform.Translate(CrossPlatformInputManager.GetAxisRaw("Horizontal") * 0, 0, MovZ * Time.deltaTime);
+        }
+        if (CrossPlatformInputManager.GetAxis("Horizontal") == -1f)
+        {
+            this.transform.Translate(CrossPlatformInputManager.GetAxisRaw("Horizontal") * 0, 0, -MovZ * Time.deltaTime);
+        }
+    }
+    // Update is called once per frame
+    /*void Update () {
         //Working on building a move horizontal method. 
         Debug.Log(Input.GetAxisRaw("Horizontal"));
         if(CrossPlatformInputManager.GetAxisRaw("Horizontal") == 1f)
@@ -24,6 +39,6 @@ public class Player : MonoBehaviour {
         {
             this.transform.Translate(CrossPlatformInputManager.GetAxisRaw("Horizontal") * 0, 0, -MovZ * Time.deltaTime);
         }
-    }
+    }*/
 
 }
