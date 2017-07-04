b8b59b22765a790c6357ebc722e1d193ea609b9a
TJ Rowe <planktime@gmail.com>
2017-07-04 16:15:59 -0600

    initial commit with
    game manager and other scripts

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+    private bool recording;
+	// Use this for initialization
+	void Start () {
+        recording = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            recording = false;
+            Debug.Log("recording is OFF");
+        }
+	}
+}
