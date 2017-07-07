79e732659adc32d967068a174b79fe9e19ace150
TJ Rowe <planktime@gmail.com>
2017-07-07 12:27:07 -0600

    wrapping this up
    May revisit the basic game with some ideas.  I have no idea what type of 2.5 d game I would make

---
+++ Assets/GameCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// I want the camera to follow the ball.  Not that this is my favorite project but it is something worth learning from.
+public class GameCamera : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        //player = GameObject.FindObjectOfType<Player>();
+        offset = transform.position - player.transform.position;
+	}
+	
+	// like update just later.
+	void LateUpdate () {
+        
+        transform.position = player.transform.position + offset;
+	}
+}
